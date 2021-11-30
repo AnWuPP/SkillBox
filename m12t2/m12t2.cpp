85164e499fc5ddf65862e2fb49ba81b896bc2c55
DonHomka <31521265+DonHomka@users.noreply.github.com>
2021-11-30 16:38:21 +0300

    Fix o(n^2)

--- m12t2/m12t2.cpp
+++ m12t2/m12t2.cpp
@@ -3,13 +3,16 @@
 int main()
 {
     int numbers[15] = { 114, 111, 106, 107, 108, 105, 115, 108, 110, 109, 112, 113, 116, 117, 118 };
-
+    int min = numbers[0];
+    int sum = 0;
     for (int i = 0; i < 15; i++) {
-        for (int j = 0; j < 15; j++) {
-            if (j != i && numbers[i] == numbers[j]) {
-                std::cout << "Duplicate number: " << numbers[i] << std::endl;
-                return 0;
-            }
-        }
+        if (min > numbers[i])
+            min = numbers[i];
+        sum += numbers[i];
     }
+    int sumSecond = 0;
+    for (int i = 0; i < 14; i++) {
+        sumSecond += min + i;
+    }
+    std::cout << sum - sumSecond << std::endl;
 }
