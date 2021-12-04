6d272087a5ed612c4a5af41a33df25897c882aa0
DonHomka <31521265+DonHomka@users.noreply.github.com>
2021-12-04 20:54:53 +0300

    compliting module 17

---
+++ m17t2/m17t2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+void reverse(int* a) {
+    for (int i = 0; i < 5; ++i) {
+        std::swap(a[i], a[9 - i]);
+    }
+}
+
+int main()
+{
+    int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    reverse(a);
+
+    for (int i = 0; i < 10; ++i) {
+        std::cout << a[i] << " ";
+    }
+    std::cout << std::endl;
+}
---
+++ m17t3/m17t3.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+bool substr(const char* a, const char* b) {
+	int i = 0;
+	bool found = false;
+	while ((*(a + i) != '\0') && !found) {
+		int j = 0;
+		bool fail = false;
+		while ((*(b + j) != '\0') && !found) {
+			fail = *(a + i + j) != *(b + j);
+			++j;
+		}
+		found = !fail;
+		++i;
+	}
+
+	return found;
+}
+
+int main()
+{
+	const char* a = "Hello world";
+	const char* b = "wor";
+	const char* c = "banana";
+	std::cout << substr(a, b) << " " << substr(a, c);
+}
