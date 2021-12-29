f07ce5fdacf7116e7835b195aba642d7e1076bb8
DonHomka <31521265+DonHomka@users.noreply.github.com>
2021-12-29 20:43:08 +0300

    fix

--- m22t3/m22t3.cpp
+++ m22t3/m22t3.cpp
@@ -2,11 +2,25 @@
 #include <string>
 #include <map>
 
-bool isAnagram(std::string& first, std::string& second) {
-	bool fail = first == second || first.length() != second.length();
+bool isAnagram(std::string& one, std::string& two) {
+	bool fail = one == two || one.length() != two.length();
 
-	for (int i = 0; i < first.length() && !fail; ++i) {
-		fail = std::count(first.begin(), first.end(), first[i]) != std::count(second.begin(), second.end(), first[i]);
+	std::map<char, int> coOne, coTwo;
+
+	for (int i = 0; i < one.length(); ++i) {
+		std::map<char, int>::iterator itOne = coOne.find(one[i]), itTwo = coTwo.find(two[i]);
+		if (itOne != coOne.end())
+			itOne->second++;
+		else
+			coOne.insert(std::pair<char, int>(one[i], 1));
+		if (itTwo != coTwo.end())
+			itTwo->second++;
+		else
+			coTwo.insert(std::pair<char, int>(two[i], 1));
+	}
+	fail = coOne.size() != coTwo.size();
+	for (std::map<char, int>::iterator it = coOne.begin(), jt = coTwo.begin(); it != coOne.end() && !fail; ++it, ++jt) {
+		fail = it->first != jt->first || it->second != jt->second;
 	}
 
 	return !fail;
@@ -14,23 +28,15 @@ bool isAnagram(std::string& first, std::string& second) {
 
 int main()
 {
-	std::map<std::string, std::string> anagrams;
 	std::string first, second;
 
 	while (first != "!q" && second != "!q") {
 		std::cout << "Input pair word: ";
 		std::cin >> first >> second;
 
-		if (isAnagram(first, second)) {
-			anagrams.insert(std::pair<std::string, std::string>(first, second));
-			std::cout << "Added pair words [" << first << " " << second << "]" << std::endl;
-		}
+		if (isAnagram(first, second))
+			std::cout << "Yea, its anagram [" << first << " " << second << "]" << std::endl;
 		else
 			std::cout << second << " is not anagram " << first << std::endl;
-	}
-
-	std::cout << "Anagrams:" << std::endl;
-	for (auto& pair : anagrams) {
-		std::cout << pair.first << " <- " << pair.second << std::endl;
 	}
 }
