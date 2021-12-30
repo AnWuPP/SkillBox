d93af68c216afb0959838d5dfe0c49e1875f0d12
DonHomka <31521265+DonHomka@users.noreply.github.com>
2021-12-30 12:02:31 +0300

    compliting module 24

---
+++ m24t1/m24t1.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+#include <ctime>
+#include <vector>
+#include <limits>
+
+struct Task {
+	std::string name = "";
+	std::time_t start = 0;
+	std::time_t end = 0;
+};
+
+void endTask(std::vector<Task>& tasks) {
+	if (tasks.size() > 0) {
+		std::vector<Task>::iterator it = --tasks.end();
+		if (it->end == 0) {
+			it->end = std::time(nullptr);
+			std::cout << "Ended task \"" << it->name << "\"" << std::endl;
+		}
+	}
+}
+
+void showTask(std::vector<Task>::iterator task) {
+	std::time_t diff = (std::time_t)std::difftime(task->end != 0 ? task->end : std::time(nullptr), task->start);
+	std::cout << "Task: " << task->name << std::endl;
+	std::printf("%s: %02d:%02d\n", (task->end != 0 ? "Time to end" : "Time from start"), diff / 60, diff % 60);
+	std::cout << "-----=========-----" << std::endl;
+}
+
+int main()
+{
+	std::string cmd;
+	std::vector<Task> tasks;
+
+	std::cout << "Enter commands: begin, end, status or exit" << std::endl;
+	while (cmd != "exit") {
+		std::cin >> cmd;
+		if (cmd == "begin") {
+			endTask(tasks);
+			Task task;
+			std::cout << "Enter name for new task: ";
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::getline(std::cin, task.name);
+			task.start = std::time(nullptr);
+
+			tasks.push_back(task);
+			std::cout << task.name << " started" << std::endl;
+		}
+		else if (cmd == "end") {
+			endTask(tasks);
+		}
+		else if (cmd == "status") {
+			if (tasks.size() > 0) {
+				for (auto it = tasks.begin(); it != --tasks.end(); ++it) {
+					if (it->end != 0) {
+						std::cout << "-----=========-----" << std::endl;
+						showTask(it);
+					}
+				}
+			}
+			if (tasks.size() == 0 || (--tasks.end())->end != 0) {
+				std::cout << "No current task" << std::endl;
+			}
+			else {
+				std::cout << "-----=========-----" << std::endl;
+				std::cout << "Current task:" << std::endl;
+				showTask(--tasks.end());
+			}
+		}
+
+	}
+
+}
---
+++ n24t3/n24t3.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <iomanip>
+
+int main()
+{
+	std::time_t now = std::time(nullptr);
+	std::tm start = *std::localtime(&now);
+	std::cin >> std::get_time(&start, "%M:%S");
+	std::time_t finish = std::time(nullptr) + (start.tm_min * 60) + start.tm_sec - 1;
+	std::time_t after = now - 1;
+	while (finish > std::time(nullptr)) {
+		if (after != std::time(nullptr)) {
+			after = std::time(nullptr);
+			std::cout << "Tick-Tack" << std::endl;
+		}
+	}
+	std::cout << "DING DING DING!!!!!!" << std::endl;
+}
