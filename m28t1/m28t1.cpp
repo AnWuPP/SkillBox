13c2630aa1cd8c61f87dcf3b52b30b7546b03f3a
DonHomka <31521265+DonHomka@users.noreply.github.com>
2022-01-18 23:33:05 +0300

    compliting task 1 & 2

---
+++ m28t1/m28t1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <thread>
+
+struct Swimmer {
+	std::string name = "Unknown";
+	double speed = 0;
+	double dist = 0;
+	std::time_t start, end;
+};
+
+int main()
+{
+	std::vector<Swimmer> swimmers(6);
+	std::vector<Swimmer> result;
+
+	std::cout << "Enter name and speed for swimmers [name speed]:" << std::endl;
+	for (auto& e : swimmers) {
+		std::cin >> e.name >> e.speed;
+	}
+
+	for (auto& e : swimmers) {
+		e.start = std::time(nullptr);
+	}
+
+	while (result.size() < 6) {
+		for (auto& e : swimmers) {
+			if (e.dist >= 100.0)
+				continue;
+			e.dist += e.speed;
+			if (e.dist > 100.0)
+				e.dist = 100.0;
+			std::cout << e.name << " distance " << e.dist << std::endl;
+			if (e.dist >= 100.0) {
+				e.end = std::time(nullptr);
+				result.push_back(e);
+			}
+		}
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+	swimmers.clear();
+
+	std::cout << "Result table:" << std::endl;
+	int count = 0;
+	for (auto& e : result) {
+		count++;
+		std::cout << "#" << count << " " << e.name << "\t" << e.end - e.start << "sec." << std::endl;
+	}
+}
---
+++ m28t2/m28t2.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <thread>
+#include <mutex>
+
+std::mutex state;
+
+class Train {
+    char id = 'N';
+    int roadTime = 0;
+public:
+    Train(char id_) : id(id_) {}
+    char getId() {
+        return id;
+    }
+    int getTime() {
+        return roadTime;
+    }
+    void setTime(int time) {
+        if (time < 0)
+            return;
+        roadTime = time;
+    }
+
+    static void move(Train*);
+};
+
+void Train::move(Train* t) {
+    std::this_thread::sleep_for(std::chrono::seconds(t->roadTime));
+    std::cout << t->getId() << " arrived" << std::endl;
+    std::lock_guard<std::mutex> m{ state };
+    std::string cmd = "";
+    std::cout << "Wait for you command for deport train " << t->getId() << std::endl;
+    while (cmd != "deport") {
+        std::cin >> cmd;
+    }
+}
+
+int main()
+{
+    std::vector<Train> trains;
+    trains.push_back(Train('A'));
+    trains.push_back(Train('B'));
+    trains.push_back(Train('C'));
+
+    int buf;
+
+    for (auto& t : trains) {
+        std::cout << "Enter road time for " << t.getId() << " train: ";
+        std::cin >> buf;
+        t.setTime(buf);
+    }
+
+    std::vector<std::thread> road(trains.size());
+
+    for (int i = 0, ie = road.size(); i != ie; ++i) {
+        road[i] = std::thread(Train::move, &trains[i]);
+    }
+
+    for (auto& t : road) {
+        t.join();
+    }
+}
