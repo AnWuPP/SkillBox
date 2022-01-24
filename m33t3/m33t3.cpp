44c0a78706666b9247a9a5a8874c83db90cafdbe
DonHomka <31521265+DonHomka@users.noreply.github.com>
2022-01-24 21:41:26 +0300

    compliting tasts 2 & 3 of module 33

---
+++ m33t3/m33t3.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T1, typename T2>
+class Container {
+    std::vector<std::pair<T1, T2>> data;
+public:
+    void add(T1 key, T2 value) {
+        data.push_back({ key, value });
+    }
+    void remove(T1 key) {
+        for (auto it = data.begin(), ite = data.end(); it != ite; ++it) {
+            if (it->first == key)
+                data.erase(it);
+        }
+    }
+    void print() {
+        for (auto& e : data) {
+            std::cout << e.first << " => " << e.second << std::endl;
+        }
+    }
+    void find(T1 key) {
+        for (auto it = data.begin(), ite = data.end(); it != ite; ++it) {
+            if (it->first == key)
+                std::cout << it->second << std::endl;
+        }
+    }
+};
+
+int main()
+{
+    Container<int, int> cont;
+    std::string cmd;
+    int a, b;
+    while (cmd != "exit") {
+        std::cin >> cmd;
+        if (cmd == "add") {
+            std::cin >> a >> b;
+            cont.add(a, b);
+        }
+        else if (cmd == "remove") {
+            std::cin >> a;
+            cont.remove(a);
+        }
+        else if (cmd == "print") {
+            cont.print();
+        }
+        else if (cmd == "find") {
+            std::cin >> a;
+            cont.find(a);
+        }
+    }
+}
---
+++ m33t4/m33t4.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+template<typename T>
+class Container {
+	std::vector<T> data;
+public:
+	void add(T val) {
+		data.push_back(val);
+	}
+	T calculate() {
+		T sum = 0;
+		for (auto& e : data) {
+			sum += e;
+		}
+		return T / static_cast<T>(data.size());
+	}
+};
+
+int main()
+{
+	Container<int> arr;
+	std::string cmd;
+	int a = 0;
+
+	while (cmd != "exit") {
+		std::cin >> cmd;
+		if (cmd == "add") {
+			std::cin >> a;
+			arr.add(a);
+		}
+		else if (cmd == "calc") {
+			std::cout << arr.calculate() << std::endl;
+		}
+	}
+}
