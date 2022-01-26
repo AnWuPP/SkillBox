c9a44fdf7b42dc61cd01ccf09b80a5f8a901fc9b
DonHomka <31521265+DonHomka@users.noreply.github.com>
2022-01-26 22:30:24 +0300

    complitings tasks

---
+++ m34t1/m34t1.cpp
@@ -0,0 +1,12 @@
+﻿// m34t1.cpp: определяет точку входа для приложения.
+//
+
+#include "m34t1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ m35t3/m35t3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <filesystem>
+
+auto recursiveGetFileNamesByExtension = [](std::filesystem::path path, const std::string extension) {
+    for (auto& file : std::filesystem::recursive_directory_iterator(path)) {
+        if (file.path().extension().compare(extension) == 0)
+            std::cout << file.path() << std::endl;
+    }
+};
+
+int main()
+{
+    std::filesystem::path p{ "D:\\Projects\\SkillBox" };
+    recursiveGetFileNamesByExtension(p, ".cpp");
+}
